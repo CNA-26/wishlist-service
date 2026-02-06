55ad3621221f81dde31c8ec54f0783f387486cab
Nea Welling <nea.welling@gmail.com>
2026-02-06 12:57:20 +0200

    Message till root :)

--- app/main.py
+++ app/main.py
@@ -4,3 +4,8 @@ from app.routers.frontendPoint import router as wishlist_router
 app = FastAPI()
 
 app.include_router(wishlist_router)
+
+@app.get("/")
+def root():
+    return {"message": "Wishlist service funkkar!"}
+
