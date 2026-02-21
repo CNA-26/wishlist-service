de644f066c48a06fff84a38e8752f224ccc2c314
Nea Welling <nea.welling@gmail.com>
2026-02-21 18:33:33 +0200

    Adda cors

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from app.routers.frontendPoint import router as wishlist_router
 from app.routers.cart import router as cart_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(wishlist_router)
 app.include_router(cart_router)
